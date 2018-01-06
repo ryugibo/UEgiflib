4ec96043dfe132587879e07dfb2da544ae92a026
류기보 <ryugibo@daum.net>
2018-01-06 20:32:53 +0900

    Texture packing (#6)
    * Add one texture, multiple sprites feature.
    
    * packing to square texture
    
    * Remove unuse parameters.

--- Source/UEgiflib/Private/GifFactory.cpp
+++ Source/UEgiflib/Private/GifFactory.cpp
@@ -61,21 +61,236 @@ UObject* UGifFactory::FactoryCreateBinary
 	const int32 Length = BufferEnd - Buffer;
 	GifIndex = 0;
 
+	/** Texture Packing Test */
 	UPaperFlipbookFactory* FlipbookFactory = NewObject<UPaperFlipbookFactory>();
 
-	TArray<UPaperSprite*> Sprites;
-	if (!DecodeGifDataToSprites(Buffer, Length, InParent, Name, Flags, Context, Type, Buffer, BufferEnd, Warn, FlipbookFactory))
+	if (!DecodeGifDataToSpritesPackedTexture((void *)Buffer, Length, InParent, Name, Flags, Context, Type, Warn, FlipbookFactory))
+	{
+		UE_LOG(LogGiflib, Error, TEXT("Failed DecodeGifDataToSpritesPackedTexture"));
+		return nullptr;
+	}
+	/*
+	if (!DecodeGifDataToSprites((void *)Buffer, Length, InParent, Name, Flags, Context, Type, Warn, FlipbookFactory))
 	{
 		UE_LOG(LogGiflib, Error, TEXT("Failed DecodeGifDataToSprites"));
 		return nullptr;
 	}
-
+	*/
 	UPaperFlipbook* Flipbook = CreateFlipbook(InParent, Name, Flags, Context, Warn, FlipbookFactory);
 
 	return Flipbook;
 }
 
-bool UGifFactory::DecodeGifDataToSprites(const void* Data, int32 Size, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8*		BufferEnd, FFeedbackContext* Warn, UPaperFlipbookFactory* FlipbookFactory)
+bool UGifFactory::DecodeGifDataToSpritesPackedTexture(void* Data, int32 Size, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, FFeedbackContext* Warn, UPaperFlipbookFactory* FlipbookFactory)
+{
+	int ErrorCode;
+	GifFileType* FileType = DGifOpen((void *)Data, UGifFactory::OnReadGif, &ErrorCode);
+	if (FileType == nullptr)
+	{
+		UE_LOG(LogGiflib, Error, TEXT("DGifOpen Failed : %d"), ErrorCode);
+		return false;
+	}
+	if (DGifSlurp(FileType) == GIF_ERROR)
+	{
+		UE_LOG(LogGiflib, Error, TEXT("DGifSlurp Failed"));
+		return false;
+	}
+
+	const int32 ImageCount = FileType->ImageCount;
+	
+	const GifWord CanvasWidth = FileType->SWidth;
+	const GifWord CanvasHeight = FileType->SHeight;
+
+	int32 RowCount = 0;
+	float RowFloat = FMath::Sqrt(ImageCount);
+	RowCount = ((RowFloat - (int32)RowFloat) > 0) ? (RowFloat + 1) : (RowFloat);
+
+	const uint32 TextureSizeX = CanvasWidth * RowCount;
+	const uint32 TextureSizeY = CanvasHeight * RowCount;
+
+	TArray<uint8> FullImage;
+	FullImage.AddUninitialized(TextureSizeX * TextureSizeY * 4);
+
+	int LastDisposalMode = 0;
+
+	TArray<FSpriteInfo> SpriteInfos;
+	SpriteInfos.AddUninitialized(ImageCount);
+
+	FSpriteInfo LastSpriteInfo;
+	for (int image_i = 0; image_i < ImageCount; image_i++)
+	{
+		SavedImage& Frame = FileType->SavedImages[image_i];
+
+		const GifWord ImageLeft = Frame.ImageDesc.Left;
+		const GifWord ImageTop = Frame.ImageDesc.Top;
+		const GifWord ImageHeight = Frame.ImageDesc.Height;
+		const GifWord ImageWidth = Frame.ImageDesc.Width;
+
+		TArray<uint8> CurrentImage;
+		CurrentImage.AddUninitialized(CanvasWidth * CanvasHeight * 4);
+
+		bool HasGCB = false;
+		GraphicsControlBlock GCB = { 0, false, 0, -1 };
+		for (int ext_i = 0; ext_i < Frame.ExtensionBlockCount; ext_i++)
+		{
+			if (GIF_OK == DGifExtensionToGCB(Frame.ExtensionBlocks[ext_i].ByteCount, Frame.ExtensionBlocks[ext_i].Bytes, &GCB))
+			{
+				HasGCB = true;
+			}
+		}
+
+		bool bIsImageExist = false;
+		switch (LastDisposalMode)
+		{
+		case DISPOSE_DO_NOT:
+			if (image_i != 0 && Frame.RasterBits[0] == '\0')
+			{
+				SpriteInfos[image_i] = LastSpriteInfo;
+				bIsImageExist = true;
+			}
+			break;
+		case DISPOSE_BACKGROUND:
+			// TODO:
+			break;
+		case DISPOSE_PREVIOUS:
+			// TODO:
+			break;
+		}
+
+		FString SourceName = FString::Printf(TEXT("%s_%d"), *Name.ToString(), image_i);
+		if (!bIsImageExist)
+		{
+			ColorMapObject* CMO = Frame.ImageDesc.ColorMap ? Frame.ImageDesc.ColorMap : FileType->SColorMap;
+			GifColorType* ColorMap = CMO->Colors;
+			uint32 Raster_i = 0;
+			uint32 Blank_i = 0;
+
+			if (Frame.ImageDesc.Interlace)
+			{
+				int InterlacedOffset[] = { 0, 4, 2, 1 };
+				int InterlacedJumps[] = { 8, 8, 4, 2 };
+
+				for (int pass_i = 0; pass_i < 4; pass_i++)
+				{
+					for (int height_i = InterlacedOffset[pass_i]; height_i < CanvasHeight; height_i += InterlacedJumps[pass_i])
+					{
+						for (int width_i = 0; width_i < CanvasWidth; width_i++)
+						{
+							if ((width_i < ImageLeft || height_i < ImageTop) ||
+								(width_i >= (ImageLeft + ImageWidth) || height_i >= (ImageTop + ImageHeight)))
+							{
+								CurrentImage[(Raster_i + Blank_i) * 4 + 0] = ColorMap[FileType->SBackGroundColor].Blue;
+								CurrentImage[(Raster_i + Blank_i) * 4 + 1] = ColorMap[FileType->SBackGroundColor].Green;
+								CurrentImage[(Raster_i + Blank_i) * 4 + 2] = ColorMap[FileType->SBackGroundColor].Red;
+								CurrentImage[(Raster_i + Blank_i) * 4 + 3] = (HasGCB && (int)FileType->SBackGroundColor == GCB.TransparentColor) ? 0 : 255;
+
+								Blank_i++;
+
+								continue;
+							}
+							unsigned char& Bit = Frame.RasterBits[Raster_i];
+							if (Bit >= CMO->ColorCount)
+							{
+								break;
+							}
+							CurrentImage[(Raster_i + Blank_i) * 4 + 0] = ColorMap[Bit].Blue;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 1] = ColorMap[Bit].Green;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 2] = ColorMap[Bit].Red;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 3] = (HasGCB && (int)Bit == GCB.TransparentColor) ? 0 : 255;
+
+							Raster_i++;
+						}
+					}
+				}
+			}
+			else
+			{
+				for (int height_i = 0; height_i < CanvasHeight; height_i++)
+				{
+					for (int width_i = 0; width_i < CanvasWidth; width_i++)
+					{
+						if ((width_i < ImageLeft || height_i < ImageTop) || 
+							(width_i >= (ImageLeft + ImageWidth) || height_i >= (ImageTop + ImageHeight)))
+						{
+							CurrentImage[(Raster_i + Blank_i) * 4 + 0] = ColorMap[FileType->SBackGroundColor].Blue;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 1] = ColorMap[FileType->SBackGroundColor].Green;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 2] = ColorMap[FileType->SBackGroundColor].Red;
+							CurrentImage[(Raster_i + Blank_i) * 4 + 3] = (HasGCB && (int)FileType->SBackGroundColor == GCB.TransparentColor) ? 0 : 255;
+
+							Blank_i++;
+
+							continue;
+						}
+						unsigned char& Bit = Frame.RasterBits[Raster_i];
+						if (Bit >= CMO->ColorCount)
+						{
+							break;
+						}
+						CurrentImage[(Raster_i + Blank_i) * 4 + 0] = ColorMap[Bit].Blue;
+						CurrentImage[(Raster_i + Blank_i) * 4 + 1] = ColorMap[Bit].Green;
+						CurrentImage[(Raster_i + Blank_i) * 4 + 2] = ColorMap[Bit].Red;
+						CurrentImage[(Raster_i + Blank_i) * 4 + 3] = (HasGCB && (int)Bit == GCB.TransparentColor) ? 0 : 255;
+
+						Raster_i++;
+					}
+				}
+			}
+
+			FIntPoint Offset((image_i % RowCount) * CanvasWidth, (image_i / RowCount) * CanvasHeight);
+			for (int height_i = 0; height_i < CanvasHeight; height_i++)
+			{
+				for (int width_i = 0; width_i < CanvasWidth; width_i++)
+				{
+					const uint32 From = 4 * (height_i * CanvasWidth + width_i);
+
+					uint32 To = (Offset.X + width_i + ((Offset.Y + height_i) * TextureSizeX)) * 4;
+
+					FullImage[To + 0] = CurrentImage[From + 0];
+					FullImage[To + 1] = CurrentImage[From + 1];
+					FullImage[To + 2] = CurrentImage[From + 2];
+					FullImage[To + 3] = CurrentImage[From + 3];
+				}
+			}
+
+			SpriteInfos[image_i].Offset = Offset;
+			SpriteInfos[image_i].Dimension = FIntPoint(CanvasWidth, CanvasHeight);
+		}
+
+		LastSpriteInfo = SpriteInfos[image_i];
+
+		if (HasGCB)
+		{
+			LastDisposalMode = GCB.DisposalMode;
+		}
+		else
+		{
+			LastDisposalMode = DISPOSAL_UNSPECIFIED;
+		}
+	}
+
+	UTexture2D* ResultTexture = CreateTextureFromRawData(FullImage, TextureSizeX, TextureSizeY, InParent, Name, Flags, Context, Warn);
+
+	for (int32 image_i = 0; image_i < SpriteInfos.Num(); image_i++)
+	{
+		FSpriteInfo Info = SpriteInfos[image_i];
+		FString SourceName = FString::Printf(TEXT("%s_%d"), *Name.ToString(), image_i);
+		UPaperSprite* NewSprite = CreatePaperSprite(InParent, *SourceName, Flags, Context, Warn, ResultTexture, Info.Offset, Info.Dimension);
+
+		FPaperFlipbookKeyFrame* KeyFrame = new (FlipbookFactory->KeyFrames) FPaperFlipbookKeyFrame();
+		KeyFrame->Sprite = NewSprite;
+		KeyFrame->FrameRun = 1;//HasGCB ? (GCB.DelayTime) : 1;
+	}
+
+	if (GIF_OK != DGifCloseFile(FileType, &ErrorCode))
+	{
+		UE_LOG(LogGiflib, Error, TEXT("DGifCloseFile Failed : %d"), ErrorCode);
+		return false;
+	}
+
+	return true;
+}
+
+bool UGifFactory::DecodeGifDataToSprites(void* Data, int32 Size, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, FFeedbackContext* Warn, UPaperFlipbookFactory* FlipbookFactory)
 {
 	int ErrorCode;
 	GifFileType* FileType = DGifOpen((void *)Data, UGifFactory::OnReadGif, &ErrorCode);
@@ -117,7 +332,6 @@ bool UGifFactory::DecodeGifDataToSprites(const void* Data, int32 Size, UObject* 
 			}
 		}
 
-		UE_LOG(LogGiflib, Log, TEXT("Delay: %d"), GCB.DelayTime);
 		UTexture2D* NewTexture = nullptr;
 		switch (LastDisposalMode)
 		{
@@ -192,8 +406,7 @@ bool UGifFactory::DecodeGifDataToSprites(const void* Data, int32 Size, UObject* 
 			NewTexture = CreateTextureFromRawData(Image, ImageWidth, ImageHeight, InParent, *SourceName, Flags, Context, Warn);
 		}
 
-		FVector2D Pivot((CanvasWidth / 2) - ImageLeft, (CanvasHeight / 2) - ImageTop);
-		UPaperSprite* NewSprite = CreatePaperSprite(NewTexture, Pivot, InParent, *SourceName, Flags, Context, Warn);
+		UPaperSprite* NewSprite = CreatePaperSprite(InParent, *SourceName, Flags, Context, Warn, NewTexture);
 
 		FPaperFlipbookKeyFrame* KeyFrame = new (FlipbookFactory->KeyFrames) FPaperFlipbookKeyFrame();
 		KeyFrame->Sprite = NewSprite;
@@ -292,7 +505,7 @@ UTexture2D* UGifFactory::CreateTextureFromRawData(const TArray<uint8>& InRawData
 	return Texture;
 }
 
-UPaperSprite* UGifFactory::CreatePaperSprite(class UTexture2D* InitialTexture, const FVector2D& Pivot, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn)
+UPaperSprite* UGifFactory::CreatePaperSprite(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn, UTexture2D* InitialTexture, const FIntPoint& InOffset, const FIntPoint& InDimension)
 {
 	FString SpriteName = ObjectTools::SanitizeObjectName(FString::Printf(TEXT("S_%s"), *Name.ToString()));
 
@@ -321,14 +534,14 @@ UPaperSprite* UGifFactory::CreatePaperSprite(class UTexture2D* InitialTexture, c
 	{
 		FSpriteAssetInitParameters SpriteInitParams;
 		SpriteInitParams.SetTextureAndFill(InitialTexture);
+		SpriteInitParams.Offset = InOffset;
+		SpriteInitParams.Dimension = InDimension;
 
 		const UPaperImporterSettings* ImporterSettings = GetDefault<UPaperImporterSettings>();
 
 		ImporterSettings->ApplySettingsForSpriteInit(SpriteInitParams, ESpriteInitMaterialLightingMode::Automatic);
 
 		NewSprite->InitializeSprite(SpriteInitParams);
-
-		NewSprite->SetPivotMode(ESpritePivotMode::Custom, Pivot);
 
 		SpritePackage->SetDirtyFlag(true);
 
