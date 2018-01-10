c6d1cb1552027ea77246b4ddd1f7e0f948709bec
류기보 <ryugibo@daum.net>
2018-01-10 09:13:56 +0900

    Support Mac Build (#11)
    * Support Mac Build
    
    * Fix crash on macos
    
    * remove unuse variables

--- Source/UEgiflib/Private/GifFactory.h
+++ Source/UEgiflib/Private/GifFactory.h
@@ -34,21 +34,22 @@ public:
 
 private:
 
-	bool DecodeGifDataToSpritesPackedTexture(void* Data, int32 Size, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, class FFeedbackContext* Warn, class UPaperFlipbookFactory* FlipbookFactory);
+	bool DecodeGifDataToSpritesPackedTexture(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, class FFeedbackContext* Warn, void* Data, class UPaperFlipbookFactory* FlipbookFactory);
 
-	bool DecodeGifDataToSprites(void* Data, int32 Size, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, class FFeedbackContext* Warn, class UPaperFlipbookFactory* FlipbookFactory);
+	bool DecodeGifDataToSprites(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, class FFeedbackContext* Warn, void* Data, class UPaperFlipbookFactory* FlipbookFactory);
 
-	class UTexture2D* CreateTextureFromRawData(const TArray<uint8>& InRawData, const GifWord& InWidth, const GifWord& InHeight, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn);
+	class UTexture2D* CreateTextureFromRawData(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn, const TArray<uint8>& InRawData, const GifWord& InWidth, const GifWord& InHeight);
 
 	class UPaperSprite* CreatePaperSprite(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn, class UTexture2D* InitialTexture, const FIntPoint& InOffset = FIntPoint::ZeroValue, const FIntPoint& InDimension = FIntPoint::ZeroValue);
 
 	class UPaperFlipbook* CreateFlipbook(UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, class FFeedbackContext* Warn, class UPaperFlipbookFactory* FlipbookFactory);
 
-	static int OnReadGif(GifFileType* FileType, GifByteType* ByteType, int Length);
+	static int OnReadGif(struct GifFileType* FileType, GifByteType* ByteType, int Length);
 
 	// * Begin - Redefine private functions of UTextureFactory
 	static bool IsImportResolutionValid(int32 Width, int32 Height, bool bAllowNonPowerOfTwo, class FFeedbackContext* Warn);
 	// * End - Redfine private functions of UTextureFactory
 
 	static int32 GifIndex;
+	static int32 GifLength;
 };
