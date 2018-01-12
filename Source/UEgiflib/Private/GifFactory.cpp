48796244097da921e5057949519adc517867acfe
ryugibo <7928632+ryugibo@users.noreply.github.com>
2018-01-12 23:42:31 +0900

    Remove some sources written by plugin template

--- Source/UEgiflib/Private/GifFactory.cpp
+++ Source/UEgiflib/Private/GifFactory.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2018 Ryugibo, Inc. All Rights Reserved.
 
 #include "GifFactory.h"
 
@@ -678,5 +678,6 @@ bool UGifFactory::IsImportResolutionValid
 
 	return bValid;
 }
+
 #undef LOCTEXT_NAMESPACE
 
