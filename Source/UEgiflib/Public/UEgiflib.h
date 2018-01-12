af6b83c2e65ac0a0efb24a60fabfc2908233109e
ryugibo <ryugibo@yahoo.com>
2018-01-12 23:42:31 +0900

    Remove some sources written by plugin template

--- Source/UEgiflib/Public/UEgiflib.h
+++ Source/UEgiflib/Public/UEgiflib.h
@@ -8,14 +8,5 @@ DECLARE_LOG_CATEGORY_EXTERN(LogGiflib, Verbose, All);
 
 class FUEgiflibModule : public IModuleInterface
 {
-public:
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
-
-private:
-
-	/** Handle to the test dll we will load */
-	// void*	ExampleLibraryHandle;
 };
